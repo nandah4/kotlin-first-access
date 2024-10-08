22d0409d1fe9c119ee3bb2dadbfb94b6936a6869
nandah4 <nyustira6@gmail.com>
2024-10-08 16:40:51 +0700

    chore: complete Kotlin controlflow  module in Start Programming with Kotlin course on Dicoding

--- src/course1_controlflow/BreakAndContinue.kt
+++ src/course1_controlflow/BreakAndContinue.kt
@@ -4,9 +4,8 @@ fun main() {
     val lisOfNumber = listOf(10, 2, null, 11, 3, 4, 22, 8)
 
     // labels using @
-    loop@ for (number in lisOfNumber) {
+    outerloop@ for (number in lisOfNumber) {
         if(number == null) continue
-        println(number)
 //        if(number == null) break
 //            println(number)
     }
